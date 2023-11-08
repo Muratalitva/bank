ff547777179578176411804ba7c4803025be8a57
Muratalieva <20adinka09@gmail.com>
2023-11-08 20:29:17 +0600

    bank

---
+++ apps/users/views.py
@@ -0,0 +1,21 @@
+from rest_framework import mixins 
+from rest_framework.viewsets import GenericViewSet
+from apps.users.models import BankUser
+from apps.users.serializers import UserSerializer, RegisterUserSerializer, UserDetailSerializer
+# Create your views here.
+
+class UserAPI(GenericViewSet,
+              mixins.ListModelMixin,
+              mixins.CreateModelMixin,
+              mixins.UpdateModelMixin,
+              mixins.RetrieveModelMixin,
+              mixins.DestroyModelMixin):
+    queryset = BankUser.objects.all()
+    serializer_class = UserSerializer
+
+    def get_serializer_class(self):
+        if self.action in ('create', ):
+            return RegisterUserSerializer
+        if self.action in ('retrieve', ):
+            return UserDetailSerializer
+        return UserSerializer
